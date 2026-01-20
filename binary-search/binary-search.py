92134238ddeb7ae4d8bd8fae2f3dda4775d145b7
Rodrigo de Lima Silva <rodrigodlima@gmail.com>
2026-01-19 22:11:08 -0300

    Fix binary search implementation to return correct index and update test case

--- binary-search/binary-search.py
+++ binary-search/binary-search.py
@@ -5,8 +5,9 @@ def binary_search(list, item):
     while low <= high:
         mid = (low + high) // 2
         guess = list[mid]
-        return guess
+        if guess == item:
+            return mid
    
 
 my_list = [1, 3, 5, 7, 9]
-print(binary_search(my_list, 3))
+print(binary_search(my_list, 5))
