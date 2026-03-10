19d73ad11cf56f87fb147251fb9f2cb25e505294
Rodrigo de Lima Silva <rodrigodlima@gmail.com>
2026-03-09 23:02:00 -0300

    Add user input functionality to search for phone numbers in the phone book

--- hash-table/hash_table.py
+++ hash-table/hash_table.py
@@ -3,5 +3,6 @@ phone_list = {}
 phone_list["Rodrigo"] = 5199708777
 phone_list["Renata"] = 51997444
 
+name = input("Enter a name to search in the phone book: ")
 
-print(phone_list["Rodrigo"])
+print(f"The phone number of {name} is:", phone_list[name])
