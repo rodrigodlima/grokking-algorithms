9193325c6118c34a04536cdb5d8fb77271328e44
Rodrigo de Lima Silva <rodrigodlima@gmail.com>
2026-03-11 19:53:02 -0300

    Add initial implementation of phone list and fruit price checker

---
+++ hash-table/hash_table02.py
@@ -0,0 +1,7 @@
+check_prices = {}
+
+check_prices["Banana"] = 4.29
+check_prices["Orange"] = 2.99
+
+name_fruit = input("Enter the name of fruit:")
+print(f"The {name_fruit} costs:", check_prices[name_fruit])
